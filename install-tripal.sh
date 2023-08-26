f5f2f81af04c5869cda2ee3a28d8dfd78a6ec4c5
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-08-27 02:52:37 +0530

    Use git clone instead of using composer to install tripal

--- install-tripal.sh
+++ install-tripal.sh
@@ -48,7 +48,7 @@ composer require drupal/field_group
 composer require drupal/field_group_table
 composer require drupal/field_formatter_class
 drush pm-enable entity views views_ui ctools ds field_group field_group_table field_formatter_class
-composer require drupal/tripal
+git clone -b 4.x https://github.com/tripal/tripal.git ./web/modules/contrib/tripal
 drush pm-enable tripal tripal_chado
 ## Chado Installation
 echo "Go to http://localhost/drupalwebsite/web/ > Tripal > Data Storage > Chado > Install Chado. Then click on Install Chado 1.3 and follow the on-screen instructions to create a job to install chado."
