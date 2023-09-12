4e625773467097e8aa51cd78bfa5c2f322adebc0
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-09-12 16:26:40 +0530

    Removing redundant portions of code from all

--- scripts/install-drush-via-phar-modules-via-drush-v4.sh
+++ scripts/install-drush-via-phar-modules-via-drush-v4.sh
@@ -24,19 +24,6 @@ function checkSMAUsername {
     [ $exitstatus -eq 1 ] && echo "Wrong Username! " && checkSMAUsername
 }
 
-# Installing dependencies and setting up base system
-sudo apt update && sudo apt upgrade -y && sudo apt install git -y
-sudo apt install apache2 -y
-cd /etc/apache2/mods-enabled && sudo ln -s ../mods-available/rewrite.load
-sudo sed -i '$i<Directory /var/www/html>\n   Options Indexes FollowSymLinks MultiViews\n   AllowOverride All\n   Order allow,deny\n   allow from all\n</Directory>' /etc/apache2/sites-available/000-default.conf
-sudo service apache2 restart
-sudo apt install php php-dev php-cli libapache2-mod-php php"$phpversion"-mbstring -y
-sudo apt install php-pgsql php-gd php-xml php-curl php-apcu php-uploadprogress -y
-read -r -p "How much memory to allocate to the website (in MB)? " memorylimit
-sudo sed -i "/memory_limit/ c\memory_limit = $memorylimit\M" /etc/php/"$phpversion"/apache2/php.ini
-sudo service apache2 restart
-sudo apt install postgresql phppgadmin wget -y
-
 # Basic database creation
 echo "___Postgres Database Creation___"
 read -r -p "Enter a new username: " psqluser
