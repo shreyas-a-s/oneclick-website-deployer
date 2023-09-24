2041a12e6aa6f57e3b1298e34cf00866d4fcf180
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-09-24 21:03:14 +0530

    Put the read memory_limit statement to top of code.

--- scripts/first.sh
+++ scripts/first.sh
@@ -3,6 +3,7 @@
 phpversion=$(apt show php | awk 'NR==2{print $2}' | awk -F ':' '{print $2}' | awk -F '+' '{print $1}')
 
 # Installing dependencies and setting up base system
+read -r -p "How much memory to allocate to the website (in MB)? " memorylimit
 sudo apt update && sudo apt upgrade -y && sudo apt install git -y
 sudo apt install apache2 -y
 cd /etc/apache2/mods-enabled && sudo ln -s ../mods-available/rewrite.load
@@ -10,7 +11,6 @@ sudo sed -i '$i<Directory /var/www/html>\n   Options Indexes FollowSymLinks Mult
 sudo service apache2 restart
 sudo apt install php php-dev php-cli libapache2-mod-php php"$phpversion"-mbstring -y
 sudo apt install php-pgsql php-gd php-xml php-curl php-apcu php-uploadprogress -y
-read -r -p "How much memory to allocate to the website (in MB)? " memorylimit
 sudo sed -i "/memory_limit/ c\memory_limit = $memorylimit\M" /etc/php/"$phpversion"/apache2/php.ini
 sudo service apache2 restart
 sudo apt install postgresql phppgadmin wget -y
