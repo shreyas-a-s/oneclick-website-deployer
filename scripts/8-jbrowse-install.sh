b7c7316232d3acd56e0d534f4884954263320554
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-09-28 19:15:01 +0530

    Add tripal_jbrowse install

--- scripts/8-jbrowse-install.sh
+++ scripts/8-jbrowse-install.sh
@@ -11,10 +11,10 @@ echo '--------------------------------'
 # Get user input
 read -r -p "Enter the name of the directory in which drupal website was installed: " drupalsitedir
 
-# Install system pre-requisites
-sudo apt update && sudo apt install build-essential zlib1g-dev unzip -y
+# Install pre-requisites
+sudo apt update && sudo apt install build-essential zlib1g-dev unzip wget curl -y
 
-# Download and setup jbrowse
+# Install jbrowse
 curl -L -O https://github.com/GMOD/jbrowse/releases/download/1.16.11-release/JBrowse-1.16.11.zip
 unzip JBrowse-1.16.11.zip
 rm JBrowse-1.16.11.zip
@@ -23,5 +23,11 @@ sudo mv JBrowse-1.16.11 "$DRUPAL_HOME"/"$drupalsitedir"/tools/jbrowse
 cd "$DRUPAL_HOME"/"$drupalsitedir"/tools/jbrowse || exit
 ./setup.sh
 
-# Test out the install
-echo "To check if the installation succeeded, go to http://localhost/""$drupalsitedir""/tools/jbrowse/"
+# Install tripal_jbrowse
+wget https://github.com/tripal/tripal_jbrowse/archive/refs/tags/7.x-3.0.zip
+unzip tripal_jbrowse-7.x-3.0.zip
+rm tripal_jbrowse-7.x-3.0.zip
+mv tripal_jbrowse-7.x-3.0 "$DRUPAL_HOME"/"$drupalsitedir"/sites/all/modules/
+mkdir -p "$DRUPAL_HOME"/"$drupalsitedir"/tools/jbrowse/data/
+sudo chown -R www-data:www-data "$DRUPAL_HOME"/"$drupalsitedir"/tools/jbrowse/data/
+drush en tripal_jbrowse_mgmt -y
