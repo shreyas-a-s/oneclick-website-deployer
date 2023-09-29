a08f7c54f2277617c3797d1b1f9ce184a8f64c53
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-09-29 23:02:32 +0530

    Update 8-jbrowse-install.sh

--- scripts/8-jbrowse-install.sh
+++ scripts/8-jbrowse-install.sh
@@ -30,5 +30,4 @@ wget https://github.com/tripal/tripal_jbrowse/archive/refs/tags/7.x-3.0.zip
 unzip 7.x-3.0.zip
 rm 7.x-3.0.zip
 mv tripal_jbrowse-7.x-3.0 "$DRUPAL_HOME"/"$drupalsitedir"/sites/all/modules/tripal_jbrowse
-drush en tripal_jbrowse_mgmt -y
-drush en tripal_jbrowse_page -y
+drush en tripal_jbrowse_mgmt tripal_jbrowse_page -y
