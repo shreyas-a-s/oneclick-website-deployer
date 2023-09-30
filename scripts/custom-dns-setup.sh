bcaaf80efca469c21d05adeb39525e375296778f
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-09-30 18:08:56 +0530

    Add error detection & correction

--- scripts/custom-dns-setup.sh
+++ scripts/custom-dns-setup.sh
@@ -10,7 +10,11 @@ sudo apt update && sudo apt install openresolv -y
 
 # Setup
 sudo sed -i '1,$d' /etc/resolv.conf && echo -e '# Custom DNS added by user\nnameserver 1.1.1.1\nnameserver 1.0.0.1\nnameserver 8.8.8.8\nnameserver 8.8.4.4' | sudo tee /etc/resolv.conf > /dev/null
-sudo sed -i "$ a/ \n# Added by user\nresolvconf=NO" /etc/resolvconf.conf
+sudo sed -i "/resolvconf=/ c\resolvconf=NO" /etc/resolvconf.conf || sudo sed -i "$ a/ \n# Added by user\nresolvconf=NO" /etc/resolvconf.conf
 
-# End
-echo "Reboot for the changes to take place."
+# Check DNS Server
+if [ $(nslookup example.com | awk 'NR==1{print $2}') = '1.1.1.1' ]; then
+  echo "Custom DNS setup Successful."
+else
+  echo "Custom DNS setup Unsuccessful. Reboot the system and check."
+fi
