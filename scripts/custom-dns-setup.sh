3520f541484ffae5112466f4d497f56e91175dcf
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-09-30 17:47:10 +0530

    Create custom-dns-setup.sh

---
+++ scripts/custom-dns-setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Display task name
+echo '---------------------'
+echo '   Custom_DNS Setup   '
+echo '---------------------'
+
+# Install dependencies
+sudo apt update && sudo apt install openresolv -y
+
+# Setup
+sudo sed -i '1,$d' /etc/resolv.conf && echo -e '# Custom DNS added by user\nnameserver 1.1.1.1\nnameserver 1.0.0.1\nnameserver 8.8.8.8\nnameserver 8.8.4.4' | sudo tee /etc/resolv.conf > /dev/null
+sudo sed -i "$ a # Added by user\nresolvconf=NO" /etc/resolvconf.conf
+
+# End
+echo "Reboot for the changes to take place."
