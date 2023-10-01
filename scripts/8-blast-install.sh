cc6f6d847e9a76d9d18d50f0ceb1b689a6896eca
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-10-02 05:11:59 +0530

    Update 8-blast-install.sh

--- scripts/8-blast-install.sh
+++ scripts/8-blast-install.sh
@@ -4,9 +4,9 @@
 sed -i '$a\DRUPAL_HOME=/var/www/html' ~/.bashrc && DRUPAL_HOME=/var/www/html
 
 # Display task name
-echo '------------------------------'
-echo '   Tripal_Blast Installation   '
-echo '------------------------------'
+echo '-----------------------'
+echo '   Blast Installation   '
+echo '------------------------'
 
 # Get user input
 read -r -p "Enter the name of the directory in which drupal website was installed: " drupalsitedir
