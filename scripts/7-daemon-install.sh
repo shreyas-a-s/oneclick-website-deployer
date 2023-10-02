253e770a8d31e655edc644004d5dd647a7e802f4
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-10-02 16:20:28 +0530

    Update 7-daemon-install.sh

--- scripts/7-daemon-install.sh
+++ scripts/7-daemon-install.sh
@@ -27,6 +27,6 @@ drush trpjob-daemon start
 
 # Set daemon to autostart during boot
 #sudo /bin/bash -c 'echo "@reboot /usr/local/bin/drush trpjob-daemon start --root=""$DRUPAL_HOME""/""$drupalsitedir" >> /etc/crontab'
-sudo touch /etc/cron.d/tripal-daemon-autostart
-echo "@reboot /usr/local/bin/drush trpjob-daemon start --root=""$DRUPAL_HOME""/""$drupalsitedir" | sudo tee -a /etc/cron.d/tripal-daemon-autostart > /dev/null
-sudo chmod 600 /etc/cron.d/tripal-daemon-autostart
+#sudo touch /etc/cron.d/tripal-daemon-autostart
+#echo "@reboot /usr/local/bin/drush trpjob-daemon start --root=""$DRUPAL_HOME""/""$drupalsitedir" | sudo tee -a /etc/cron.d/tripal-daemon-autostart > /dev/null
+#sudo chmod 600 /etc/cron.d/tripal-daemon-autostart
