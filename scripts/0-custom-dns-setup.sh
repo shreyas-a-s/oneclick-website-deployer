76f3962838694dcd2654b394c9518c96487c6403
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-10-02 22:40:26 +0530

    Fix /etc/resolvconf.conf not getting appended

--- scripts/0-custom-dns-setup.sh
+++ scripts/0-custom-dns-setup.sh
@@ -10,7 +10,12 @@ sudo apt-get update && sudo apt-get -y install openresolv
 
 # Setup
 sudo sed -i '1,$d' /etc/resolv.conf && echo -e '# Custom DNS added by user\nnameserver 1.1.1.1\nnameserver 1.0.0.1\nnameserver 8.8.8.8\nnameserver 8.8.4.4' | sudo tee /etc/resolv.conf > /dev/null
-sudo sed -i "/resolvconf=/ c\resolvconf=NO" /etc/resolvconf.conf || sudo sed -i "$ a/ \n# Added by user\nresolvconf=NO" /etc/resolvconf.conf
+grep 'resolvconf=' /etc/resolvconf.conf > /dev/null
+if [ $? -eq 0 ]; then
+  sudo sed -i "/resolvconf=/ c\resolvconf=NO" /etc/resolvconf.conf
+else
+  sudo sed -i "$ a\ \n# Added by user\nresolvconf=NO" /etc/resolvconf.conf
+fi
 
 # Check DNS Server
 if [ "$(nslookup example.com | awk 'NR==1{print $2}')" = '1.1.1.1' ]; then
