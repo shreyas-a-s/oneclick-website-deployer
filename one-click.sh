36f36b6e0e0415ba6fd1e993e6fe1810c0cf030f
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-10-03 20:26:50 +0530

    Update one-click.sh

--- one-click.sh
+++ one-click.sh
@@ -8,20 +8,13 @@ sudo apt-get -y install git
 temp_dir=$(mktemp -d)
 
 # Clone the GitHub repository into the temporary directory
-git clone -b dev-3 https://github.com/shreyas-a-s/website-tripal.git "$temp_dir"
+git clone -b 3 https://github.com/shreyas-a-s/website-tripal.git "$temp_dir"
 
 # Change directory to the temporary folder
 cd "$temp_dir" || exit
 
 # Run the install.sh script
-./scripts/1-base-setup.sh
-./scripts/2-drush-install.sh
-./scripts/3-postgres-setup.sh
-./scripts/4-drupal-install.sh
-./scripts/5-tripal-install.sh
-./scripts/6-blast-install.sh
-./scripts/7-daemon-install.sh
-./scripts/8-jbrowse-install.sh
+./install.sh
 
 # Clean up: Remove the temporary directory and its contents
 cd .. || exit
