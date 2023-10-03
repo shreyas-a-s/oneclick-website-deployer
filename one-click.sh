56bf51c74bab05cd846d09ce0fbec3f10c52984e
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-10-03 20:05:50 +0530

    Update one-click.sh

--- one-click.sh
+++ one-click.sh
@@ -14,14 +14,7 @@ git clone -b dev-3 https://github.com/shreyas-a-s/website-tripal.git "$temp_dir"
 cd "$temp_dir" || exit
 
 # Run the install.sh script
-./scripts/1-base-setup.sh
-./scripts/2-drush-install.sh
-./scripts/3-postgres-setup.sh
-./scripts/4-drupal-install.sh
-./scripts/5-tripal-install.sh
-./scripts/6-blast-install.sh
-./scripts/7-daemon-install.sh
-./scripts/8-jbrowse-install.sh
+./install.sh
 
 # Clean up: Remove the temporary directory and its contents
 cd .. || exit
