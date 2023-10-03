ebcfa49f350cf29be6f612485c064288bc6a4b5d
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-10-03 19:14:28 +0530

    Automate setting up drupal cron tasks

--- scripts/5-cron-automation.sh
+++ scripts/5-cron-automation.sh
@@ -9,13 +9,6 @@ echo '+---------------------------+'
 read -r -p "Enter the name of the directory in which drupal website was installed: " drupalsitedir
 
 # Installation
+echo "0,30 * * * * $USER /usr/local/bin/drush core-cron --root=$DRUPAL_HOME/$drupalsitedir" | sudo tee /etc/cron.d/drupal-cron-tasks > /dev/null
 echo "1. Go to http://localhost/""$drupalsitedir""/admin/config/system/cron"
 echo "2. Set the drop down value titled 'Run cron every' to 'Never' and save the configuration."
-echo "3. Also, on that page is the URL for cron."
-echo "- Example URL: http://localhost/cron.php?cron_key=pnwI1cni8wjX1tVPOBaAJmoGOrzDsFQCW_7pwVHhigE"
-echo "4. Execute sudo crontab -e from a terminal."
-echo "5. Choose option [1] /bin/nano"
-echo "6. Add this line as last line: 0,30 * * * * /usr/bin/wget -O - -q http://localhost/cron.php?cron_key=pnwI1cni8wjX1tVPOBaAJmoGOrzDsFQCW_7pwVHhigE"
-echo "7. Ctrl + O"
-echo "8. Enter"
-echo "9. Ctrl + X"
