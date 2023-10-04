10e154e0aca5dcaed0a7fec836b176ef28c9b828
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-10-04 22:19:34 +0530

    Delete 0-hosts-setup.sh

--- scripts/0-hosts-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Display task name
-echo -e '\n+-----------------+'
-echo '|   Hosts Setup   |'
-echo '+-----------------+'
-
-# Setup
-echo "185.199.108.133 raw.githubusercontent.com" | sudo tee -a /etc/hosts
