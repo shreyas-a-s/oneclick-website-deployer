eea4c8b67a8b1d2df58cbcf9c0954e2023e4b119
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-10-04 22:20:53 +0530

    Change method to installing resolvconf and setting /etc/resolvconf/resolv.conf.d/head

--- scripts/0-custom-dns-setup.sh
+++ scripts/0-custom-dns-setup.sh
@@ -6,20 +6,10 @@ echo '|   Custom DNS Setup   |'
 echo '+----------------------+'
 
 # Install dependencies
-sudo apt-get update && sudo apt-get -y install openresolv
+sudo apt-get update && sudo apt-get -y install resolvconf
 
 # Setup
-sudo sed -i '1,$d' /etc/resolv.conf && echo -e '# Custom DNS added by user\nnameserver 1.1.1.1\nnameserver 1.0.0.1\nnameserver 8.8.8.8\nnameserver 8.8.4.4' | sudo tee /etc/resolv.conf > /dev/null
-grep 'resolvconf=' /etc/resolvconf.conf > /dev/null
-if [ $? -eq 0 ]; then
-  sudo sed -i "/resolvconf=/ c\resolvconf=NO" /etc/resolvconf.conf
-else
-  sudo sed -i "$ a\ \n# Added by user\nresolvconf=NO" /etc/resolvconf.conf
-fi
+sudo sed -i "$ a nameserver 1.1.1.1" /etc/resolvconf/resolv.conf.d/head
 
-# Check DNS Server
-if [ "$(nslookup example.com | awk 'NR==1{print $2}')" = '1.1.1.1' ]; then
-  echo "Custom DNS setup Successful."
-else
-  echo "Custom DNS setup Unsuccessful. Reboot the system and check."
-fi
+# End
+echo "Reboot the system for new DNS settings to take effect."
