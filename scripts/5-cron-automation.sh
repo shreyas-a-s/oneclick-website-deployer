9fae892399b45562da772bde67f3a7c3494a3de7
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-10-12 16:05:09 +0530

    continueORNot after cron

--- scripts/5-cron-automation.sh
+++ scripts/5-cron-automation.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Take user choice before continuing
+function continueORNot {
+   read -r -p "Continue? (yes/no): " choice
+   case "$choice" in 
+     "yes" ) echo "Moving on to next step..";;
+     "no" ) echo "Exiting.."; exit 1;;
+     * ) echo "Invalid Choice! Keep in mind this is case-sensitive."; continueORNot;;
+   esac
+}
+
 # Display task name
 echo -e '\n+---------------------------+'
 echo '|   Cron Automation Setup   |'
@@ -14,4 +24,4 @@ fi
 echo "0,30 * * * * $USER /usr/local/bin/drush core-cron --root=$DRUPAL_HOME/$drupalsitedir" | sudo tee /etc/cron.d/drupal-cron-tasks > /dev/null
 echo "1. Go to http://localhost/""$drupalsitedir""/admin/config/system/cron"
 echo "2. Set the drop down value titled 'Run cron every' to 'Never' and save the configuration."
-sleep 10
+continueORNot
