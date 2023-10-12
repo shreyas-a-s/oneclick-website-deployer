14d31234eb93980a9b15564d8351fd645bee9d18
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-10-13 01:01:28 +0530

    Automate & Create whiptail msgbox for drupal-install

--- scripts/4-drupal-install.sh
+++ scripts/4-drupal-install.sh
@@ -40,15 +40,8 @@ echo '|   Site Setup   |'
 echo '+----------------+'
 sed -i "s/\$databases\ =\ array();/\n\$databases['default']['default']\ =\ array(\n\t'driver'\ =>\ 'pgsql',\n\t'database'\ =>\ '$psqldb',\n\t'username'\ =>\ '$psqluser',\n\t'password'\ =>\ '$PGPASSWORD_ESCAPED',\n\t'host'\ =>\ 'localhost',\n\t'prefix'\ =>\ '',\n);/" sites/default/settings.php
 while ! ({ drush variable-get --root="$DRUPAL_HOME"/"$drupalsitedir" | grep -q drupal; } &> /dev/null && sleep 2); do
-  : # Do nothing, just continue the loop until drupal variable is added to drush's variable list
+whiptail --title "Drupal Setup" --msgbox --ok-button "" --notags "1. Go to http://localhost/""$drupalsitedir""/install.php\n2. Ensure that 'Standard' option is selected and click 'Save and continue'.\n3. Select the language you want to choose. Choose 'English'.\n4. You will see a progress bar as Drupal is installed.\n5. Once it completes, a configuration page will be visible.\n6. Provide details appropriate to your site and note down the Site Maintenance Account details.\n7. Click 'Save and continue.'" 15 80;
 done
-echo "1. Go to http://localhost/""$drupalsitedir""/install.php"
-echo "2. Ensure that 'Standard' option is selected and click 'Save and continue'."
-echo "3. You will next be asked to select the language you want to choose. Choose 'English'."
-echo "4. You will see a progress bar as Drupal is installed."
-echo "5. Once it completes, a configuration page with some final settings will be visible."
-echo "6. Provide details appropriate to your site and note down the Site Maintenance Account details."
-echo "7. Click 'Save and continue.'"
 sudo chmod 755 sites/default/settings.php
 
 # Webform Module (for creating Google-form-like Forms)
