8ec3b70d9f818eef9f426b53f044e58a47d322ad
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-10-13 02:03:10 +0530

    Create msgbox for sample blast db setup

--- scripts/8-blast-install.sh
+++ scripts/8-blast-install.sh
@@ -62,10 +62,4 @@ drush variable-set blast_path "$DRUPAL_HOME"/"$drupalsitedir"/tools/blast/bin/ -
 echo -e '\n+---------------------------+'
 echo '|   Sample Blast DB Setup   |'
 echo '+---------------------------+'
-echo "1. Go to http://localhost/""$drupalsitedir""/node/add/blastdb"
-echo "2. Fill out the form like this:"
-echo "- Human-readable Name: 16S_ribosomal_RNA"
-echo "- File Prefix including Full Path: ""$DRUPAL_HOME""/""$drupalsitedir""/tools/blast/blastdb/16S_ribosomal_RNA/16S_ribosomal_RNA"
-echo "- Set 'Type of the blast database' to Nucleotide."
-echo "- Got to bottom of page and click 'Save'"
-echo "3. Go to http://localhost/""$drupalsitedir""/blast/nucleotide/nucleotide and test out the blast install by Entering a FASTA sequence (or uploading one) & Selecting the newly added Database from dropdown under Nucleotide BLAST Databases & Clicking 'BLAST'"
+whiptail --title "Sample Blast DB Setup" --msgbox --ok-button "OK" --notags "1. Go to http://localhost/""$drupalsitedir""/node/add/blastdb\n2. Fill out the form like this:\n- Human-readable Name: 16S_ribosomal_RNA\n- File Prefix including Full Path: ""$DRUPAL_HOME""/""$drupalsitedir""/tools/blast/blastdb/16S_ribosomal_RNA/16S_ribosomal_RNA\n- Set 'Type of the blast database' to Nucleotide.\n3. Got to bottom of page and click 'Save'\n4. Go to http://localhost/""$drupalsitedir""/blast/nucleotide/nucleotide and test out the blast install by Entering a FASTA sequence (or uploading one) & Selecting the newly added Database from dropdown under Nucleotide BLAST Databases & Clicking 'BLAST'\n5. Hit 'OK' after completing these steps." 18 78
