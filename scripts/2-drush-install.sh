fa8f1d4960b934d97570909ab82553d0be1032b1
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-10-12 14:33:53 +0530

    Fix for "The PHP error_log at  is not writable!"

--- scripts/2-drush-install.sh
+++ scripts/2-drush-install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Variables
+phpversion=$(apt-cache show php | grep version | awk '{print $4}' | awk -F ')' '{print $1}')
+
 # Display task name
 echo -e '\n+------------------------+'
 echo '|   Drush Installation   |'
@@ -9,3 +12,7 @@ echo '+------------------------+'
 wget https://github.com/drush-ops/drush/releases/download/8.4.12/drush.phar
 chmod +x drush.phar
 sudo mv drush.phar /usr/local/bin/drush
+
+# Fix for "The PHP error_log at  is not writable!"
+sudo touch /var/log/php-errors.log
+sudo sed -i "/error_log\ =\ php/ c\error_log\ =\ /var/log/php-errors.log" /etc/php/"$phpversion"/cli/php.ini
