4552abb7298a9e643ac8912b383fedae5b9e3239
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-10-13 01:44:50 +0530

    Add msgbox to cron-automation

--- scripts/5-cron-automation.sh
+++ scripts/5-cron-automation.sh
@@ -22,6 +22,4 @@ fi
 
 # Installation
 echo "0,30 * * * * $USER /usr/local/bin/drush core-cron --root=$DRUPAL_HOME/$drupalsitedir" | sudo tee /etc/cron.d/drupal-cron-tasks > /dev/null
-echo "1. Go to http://localhost/""$drupalsitedir""/admin/config/system/cron"
-echo "2. Set the drop down value titled 'Run cron every' to 'Never' and save the configuration."
-continueORNot
+whiptail --title "Cron Run" --msgbox --ok-button "OK" --notags "1. Go to http://localhost/""$drupalsitedir""/admin/config/system/cron\n2. Set the drop down value titled 'Run cron every' to 'Never' and save the configuration.\n3. Hit 'OK' after completing these steps." 10 65
