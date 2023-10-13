ae70ebd120b59fbfb8109eee8a8afa81b5ab9a44
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-10-13 12:52:23 +0530

    Fix double quotes

--- scripts/5-cron-automation.sh
+++ scripts/5-cron-automation.sh
@@ -13,4 +13,4 @@ fi
 # Installation
 echo "0,30 * * * * $USER /usr/local/bin/drush core-cron --root=$DRUPAL_HOME/$drupalsitedir" | sudo tee /etc/cron.d/drupal-cron-tasks > /dev/null
 whiptail --title "Cron Run" --msgbox --ok-button "OK" --notags "1. Go to http://localhost/""$drupalsitedir""/admin/config/system/cron\n2. Set the drop down value titled 'Run cron every' to 'Never' and save the configuration.\n3. Hit 'OK' after completing these steps." 10 65
-drush core-cron --root=$DRUPAL_HOME/$drupalsitedir"
+drush core-cron --root="$DRUPAL_HOME"/"$drupalsitedir"
