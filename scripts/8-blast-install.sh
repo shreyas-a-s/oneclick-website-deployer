a5ace6e0d36dcfb305c4c5cb6ce7e9e459a8dc1c
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-10-13 18:53:39 +0530

    Display setup complete after sample blast setup

--- scripts/8-blast-install.sh
+++ scripts/8-blast-install.sh
@@ -63,3 +63,4 @@ echo -e '\n+---------------------------+'
 echo '|   Sample Blast DB Setup   |'
 echo '+---------------------------+'
 whiptail --title "Sample Blast DB Setup" --msgbox --ok-button "OK" --notags "1. Go to http://localhost/""$drupalsitedir""/node/add/blastdb\n2. Fill out the form like this:\n- Human-readable Name: 16S_ribosomal_RNA\n- File Prefix including Full Path: ""$DRUPAL_HOME""/""$drupalsitedir""/tools/blast/blastdb/16S_ribosomal_RNA/16S_ribosomal_RNA\n- Set 'Type of the blast database' to Nucleotide.\n3. Got to bottom of page and click 'Save'\n4. After the installation is completed and system is rebooted, you can go to http://localhost/""$drupalsitedir""/blast/nucleotide/nucleotide and test out blast by Entering a FASTA sequence (or uploading one) & Selecting the newly added Database from dropdown under Nucleotide BLAST Databases & Clicking 'BLAST'\n5. Hit 'OK' after completing these steps." 18 78
+echo "Setup complete."
