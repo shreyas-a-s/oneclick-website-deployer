7c815f20f96db3b648a790b3dbcb3992760cba8e
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-10-13 18:22:36 +0530

    Make cron job independed of both "smausername" and "trp-run-jobs"

--- scripts/5-cron-automation.sh
+++ scripts/5-cron-automation.sh
@@ -3,15 +3,6 @@
 # Variables
 DRUPAL_HOME=/var/www/html
 
-# Test site maintenance username
-function checkSMAUsername {
-    titleSMA="Site Maintenance Account"
-    while ! ( { drush user-information "$smausername" --root="$DRUPAL_HOME"/"$drupalsitedir" | grep -q administrator; } &> /dev/null ); do
-        smausername=$(whiptail --title "$titleSMA" --inputbox "\nEnter the site maintenance account username that you've given to the website: " 10 48 3>&1 1>&2 2>&3)
-        titleSMA="Wrong Username"
-    done
-}
-
 # Display task name
 echo -e '\n+---------------------------+'
 echo '|   Cron Automation Setup   |'
@@ -23,7 +14,6 @@ if [[ -z ${drupalsitedir} ]]; then
 fi
 
 # Installation
-checkSMAUsername && export smausername
-echo "0,30 * * * * $USER /usr/local/bin/drush core-cron --root=$DRUPAL_HOME/$drupalsitedir && /usr/local/bin/drush trp-run-jobs --username=$smausername --root=$DRUPAL_HOME/$drupalsitedir" | sudo tee /etc/cron.d/drupal-cron-tasks > /dev/null
+echo "0,30 * * * * $USER /usr/bin/wget -O - -q http://localhost/$drupalsitedir/cron.php?cron_key=$(drush variable-get cron_key --root=$DRUPAL_HOME/$drupalsitedir | awk '{print $2}')" | sudo tee /etc/cron.d/drupal-cron-tasks > /dev/null
 whiptail --title "Cron Run" --msgbox --ok-button "OK" --notags "1. Go to http://localhost/""$drupalsitedir""/admin/config/system/cron\n2. Set the drop down value titled 'Run cron every' to 'Never' and save the configuration.\n3. Hit 'OK' after completing these steps." 10 65
-drush core-cron --root="$DRUPAL_HOME"/"$drupalsitedir"
+wget -O - -q http://localhost/"$drupalsitedir"/cron.php?cron_key=$(drush variable-get cron_key --root="$DRUPAL_HOME"/"$drupalsitedir" | awk '{print $2}')
