9f68a43d77e7875739620aa2c35b95de1baf1c8f
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-10-13 13:28:41 +0530

    Add whiptail colorscheme stolen from @op

--- install.sh
+++ install.sh
@@ -9,6 +9,30 @@ echo "╚███╔███╔╝███████╗██████�
 echo " ╚══╝╚══╝ ╚══════╝╚═════╝ ╚══════╝╚═╝   ╚═╝   ╚══════╝    ╚═╝   ╚═╝  ╚═╝╚═╝╚═╝     ╚═╝  ╚═╝╚══════╝"
 echo -e "---------------------------------------------------------------------------------------------------"
 
+# whiptail colorscheme stolen from https://github.com/op/whiplash/blob/master/lamenting
+export NEWT_COLORS='
+root=white,gray
+window=white,lightgray
+border=black,lightgray
+shadow=white,black
+button=black,green
+actbutton=black,red
+compactbutton=black,
+title=black,
+roottext=black,magenta
+textbox=black,lightgray
+acttextbox=gray,white
+entry=lightgray,gray
+disentry=gray,lightgray
+checkbox=black,lightgray
+actcheckbox=black,green
+emptyscale=,black
+fullscale=,red
+listbox=black,lightgray
+actlistbox=lightgray,gray
+actsellistbox=black,green
+'
+
 # Variables
 debianversion=$(awk -F '.' '{print $1}' < /etc/debian_version)
 
