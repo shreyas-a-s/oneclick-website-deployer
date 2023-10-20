e3547e021dbaa40155bccb88f342bb9dbf58bfa4
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-10-20 23:16:35 +0530

    Make the fix better

--- scripts/0-custom-dns-setup.sh
+++ scripts/0-custom-dns-setup.sh
@@ -5,11 +5,17 @@ echo -e '\n+----------------------+'
 echo '|   Custom DNS Setup   |'
 echo '+----------------------+'
 
+# Get the name of network interface that is UP
+interfacename=$(ip link show | grep 'state UP' | awk '{print $2}' | awk -F ':' '{print $1}')
+
 # Install dependencies
 sudo apt-get update && sudo apt-get -y install resolvconf
 
 # Setup
 sudo sed -i "$ a nameserver 1.1.1.1" /etc/resolvconf/resolv.conf.d/head
 
-# End
-echo "Reboot the system for new DNS settings to take effect."
+# Deactivate network interface
+sudo ifdown $interfacename
+
+# Reactivate network interface
+sudo ifup $interfacename
