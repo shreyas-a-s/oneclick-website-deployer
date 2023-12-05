12dfebb22e87eb7d274503c542a690adee0632de
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-06 00:03:09 +0530

    Properly fix 'Trying to access array offset on value of type null in field_formatter_settings_get_instance_display_settings'

--- scripts/6-tripal-install.sh
+++ scripts/6-tripal-install.sh
@@ -96,5 +96,8 @@ if [ "$current_network_config" != "$initial_network_config" ]; then
 fi
 
 # Fix for "Trying to access array offset on value of type null" error that gets displayed
-# when we refresh home page after adding some menu links
-sed -i "s/return \$defaults/return \$defaults = NULL/" sites/all/modules/field_formatter_settings/field_formatter_settings.module
+# when we refresh overlay menus (eg: localhost/drupal/bio_data/1#overlay-context=&overlay=admin/tripal)
+# source: https://www.drupal.org/project/field_formatter_settings/issues/3166628
+cd "$DRUPAL_HOME"/"$drupalsitedir"/sites/all/modules/field_formatter_settings || exit
+patch -p1 < $SCRIPT_DIR/field_formatter_settings.patch
+
