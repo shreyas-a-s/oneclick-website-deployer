0c2a96515f4b1c539425f4ab4e42f42419460676
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-24 15:57:37 +0530

    Fix - whiptail not getting installed in debian 11

--- install.sh
+++ install.sh
@@ -50,7 +50,7 @@ if ! command -v apt-get > /dev/null; then
     no|n|NO)
       exit 1 ;;
   esac
-elif [ "$(awk -F '.' '{print $1}' < /etc/debian_version)" -ne 11 ] && sudo apt update && sudo apt install -y whiptail && ! whiptail --title "Warning" --yesno --yes-button "Continue" --no-button "Cancel" "Drupal 7 (and thereby Tripal 3) works best in Debian 11. This system is Debian $(awk -F '.' '{print $1}' < /etc/debian_version). Installation might not work properly in this system. But you can continue with the installation if you want." 10 56; then
+elif sudo apt-get update && sudo apt-get install -y whiptail && [ "$(awk -F '.' '{print $1}' < /etc/debian_version)" -ne 11 ] && ! whiptail --title "Warning" --yesno --yes-button "Continue" --no-button "Cancel" "Drupal 7 (and thereby Tripal 3) works best in Debian 11. This system is Debian $(awk -F '.' '{print $1}' < /etc/debian_version). Installation might not work properly in this system. But you can continue with the installation if you want." 10 56; then
   exit 1
 fi
 
