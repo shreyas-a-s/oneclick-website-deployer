c1eaa830ca25be3889cba197036fccdae44ee6b8
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-25 20:14:27 +0530

    Update the locations of scripts in install.sh

--- install.sh
+++ install.sh
@@ -24,19 +24,19 @@ SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 ./scripts/pre-deployment/get-user-input.sh            # Get user input
 
 # Deployment scripts
-./scripts/1-base-setup.sh
-./scripts/2-drush-install.sh
-./scripts/3-postgres-setup.sh
-./scripts/4-drupal-install.sh
-./scripts/5-cron-automation.sh
-./scripts/6-tripal-install.sh
-./scripts/7-daemon-install.sh
-./scripts/8-blast-install.sh
-./scripts/9-jbrowse-install.sh
+./scripts/deployment/1-base-setup.sh
+./scripts/deployment/2-drush-install.sh
+./scripts/deployment/3-postgres-setup.sh
+./scripts/deployment/4-drupal-install.sh
+./scripts/deployment/5-cron-automation.sh
+./scripts/deployment/6-tripal-install.sh
+./scripts/deployment/7-daemon-install.sh
+./scripts/deployment/8-blast-install.sh
+./scripts/deployment/9-jbrowse-install.sh
 
 # Unset PGPASSWORD
 unset PGPASSWORD
 
 # Post-deployment scripts
-./choice-to-reboot.sh  # Give user a choice of whether to automatically reboot the system or not
+./scripts/post-deployment/choice-to-reboot.sh  # Give user a choice of whether to automatically reboot the system or not
 
