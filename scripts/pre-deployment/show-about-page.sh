4f94dc6ce90d90319e1fdba015840b9f9fc4e582
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-25 21:16:50 +0530

    Make show-about-page.sh efficient

--- scripts/pre-deployment/show-about-page.sh
+++ scripts/pre-deployment/show-about-page.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-whiptail --title "About The Script" --textbox --ok-button "Continue" ./about.txt 20 86
+if command -v whiptail > /dev/null; then # Display 'About Us' using whiptail
+  if [ -f ./about.txt ]; then # Checking is about.txt is present
+    whiptail --title "About The Script" --textbox --ok-button "Continue" ./about.txt 20 86
+  fi
+else # Display 'About Us' using command-line
+  if [ -f ./about.txt ]; then # Checking is about.txt is present
+    cat ./about.txt
+  fi
+fi
+
