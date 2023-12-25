1a99f4d59c9dea2b484f5f4bbdea3b273765a36b
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-25 15:07:07 +0530

    Separate 'checking is os is supported' into child script

--- install.sh
+++ install.sh
@@ -22,16 +22,8 @@ SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 # Display info
 whiptail --title "About The Script" --textbox --ok-button "Continue" about.txt 20 86
 
-# Give warning if OS is not debian
-if ! command -v apt-get > /dev/null; then
-  printf "This doesn't seem to be a Debian Linux system. Installation might not work properly. But you can continue with the installation if you want. Continue (yes/no): " && read -r continue_or_not
-  case $continue_or_not in
-    no|n|NO)
-      exit 1 ;;
-  esac
-elif sudo apt-get update && sudo apt-get install -y whiptail && [ "$(awk -F '.' '{print $1}' < /etc/debian_version)" -ne 11 ] && ! whiptail --title "Warning" --yesno --yes-button "Continue" --no-button "Cancel" "Drupal 7 (and thereby Tripal 3) works best in Debian 11. This system is Debian $(awk -F '.' '{print $1}' < /etc/debian_version). Installation might not work properly in this system. But you can continue with the installation if you want." 10 56; then
-  exit 1
-fi
+# Check if the Operating System is supported
+./check-if-os-is-supported.sh
 
 # Get user input
 while true; do
