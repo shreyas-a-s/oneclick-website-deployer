3835872faaa42372454d09da95ef658769d197c9
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-26 19:18:33 +0530

    Remove unnecessary empty lines

--- scripts/pre-deployment/components/get-db-creds.sh
+++ scripts/pre-deployment/components/get-db-creds.sh
@@ -4,20 +4,16 @@ if command -v whiptail > /dev/null; then
   # Read database name
   psqldb=$(whiptail --title "User Input" --inputbox "\nEnter the name for a new database for our website: " 10 47 "$psqldb" 3>&1 1>&2 2>&3)
   export psqldb
-
   # Read database username
   psqluser=$(whiptail --title "User Input" --inputbox "\nEnter a new username (role) for postgres: " 9 45 "$psqluser" 3>&1 1>&2 2>&3)
   export psqluser
-
   # Read database password
   PGPASSWORD=$(whiptail --title "User Input" --passwordbox "\nEnter a password for the new user: " 9 38 "$PGPASSWORD" 3>&1 1>&2 2>&3)
 else
   # Read database name
   read -r -p "Enter the name for a new database for our website: " psqldb
-  
   # Read database username
   read -r -p "Enter a new username (role) for postgres: " psqluser
-  
   # Read database password
   read -r -p "Enter a password for the new user: " PGPASSWORD
 fi
