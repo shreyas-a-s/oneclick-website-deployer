f880a29b90120b18880fa70ba96ea7615e01818f
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-26 19:13:54 +0530

    Separate 'read directory name to install drupal' into separate script

--- scripts/pre-deployment/get-user-input.sh
+++ scripts/pre-deployment/get-user-input.sh
@@ -8,8 +8,7 @@ while true; do
   . ./scripts/pre-deployment/components/get-db-creds.sh
 
   # Read drupal directory name
-  drupalsitedir=$(whiptail --title "User Input" --inputbox "\nEnter the name of the directory to which drupal website needs to be installed: " 10 45 "$drupalsitedir" 3>&1 1>&2 2>&3)
-  export drupalsitedir
+  . ./scripts/pre-deployment/components/get-drupal-dir.sh
 
   # Give option to edit user input
   if (whiptail --defaultno --yesno "Do you want to edit the data?\n\nMemory Limit: $memorylimit""MB\nDatabase Name: $psqldb\nDatabase User: $psqluser\nDatabase Password: $HIDDEN_PGPASSWORD\nDrupal Website Directory: $drupalsitedir" 14 50) then
