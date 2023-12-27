845942f014e81c12cf327d16cf65ab756b7b5471
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-27 15:19:11 +0530

    Separate install-chado.sh from install-tripal

--- scripts/deployment/6-tripal-install.sh
+++ scripts/deployment/6-tripal-install.sh
@@ -35,18 +35,7 @@ drush pm-enable -y tripal tripal_chado tripal_ds tripal_ws
 _test_sma_username
 
 # Chado installation
-echo -e '\n+----------------------+'
-echo '|   Installing Chado   |'
-echo '+----------------------+'
-while [[ $(drush variable-get --root="$DRUPAL_HOME"/"$drupalsitedir" | grep chado_schema_exists | awk '{print $2}') == "true" ]]; do
-  whiptail --title "Install Chado" --msgbox --ok-button "OK" --notags "1. Go to http://localhost/""$drupalsitedir""/admin/tripal/storage/chado/install\n2. Click the drop-down menu under Installation/Upgrade.\n3. Select 'New Install of Chado v1.3'.\n4. Click 'Install/Upgrade Chado'.\n-  NOTE: THERE IS NO NEED TO RUN THE DRUSH COMMAND.\n5. Hit 'OK' after completing these steps." 13 65
-  drush trp-run-jobs --username="$smausername" --root="$DRUPAL_HOME"/"$drupalsitedir" &> /dev/null
-done
-if [ $? -eq 0 ]; then
-  echo "Chado Installation Successful."
-else
-  echo "Chado Installation Failed."
-fi
+.SCRIPT_DIR/components/install-chado.sh
 
 # Chado preparation
 echo -e '\n+---------------------+'
