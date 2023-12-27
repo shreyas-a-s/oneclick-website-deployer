dd1f982fbc0936d74bea9eb49153e1006431cc5f
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-27 16:16:52 +0530

    minor edits

--- scripts/deployment/9-jbrowse-install.sh
+++ scripts/deployment/9-jbrowse-install.sh
@@ -14,7 +14,9 @@ if [[ -z ${drupalsitedir} ]]; then
 fi
 
 # Install pre-requisites
-sudo apt-get update && sudo apt-get -y install build-essential zlib1g-dev unzip wget curl
+if command -v apt-get > /dev/null; then
+  sudo apt-get -y install build-essential zlib1g-dev unzip wget curl
+fi
 
 # Install jbrowse
 curl -L -O https://github.com/GMOD/jbrowse/releases/download/1.16.11-release/JBrowse-1.16.11.zip
@@ -34,8 +36,13 @@ unzip 7.x-3.0.zip
 rm 7.x-3.0.zip
 mv tripal_jbrowse-7.x-3.0 "$DRUPAL_HOME"/"$drupalsitedir"/sites/all/modules/tripal_jbrowse
 drush pm-enable -y tripal_jbrowse_mgmt tripal_jbrowse_page
+
+# Setup tripal_jbrowse
 echo -e '\n+--------------------------+'
 echo '|   Tripal_JBrowse Setup   |'
 echo '+--------------------------+'
 drush variable-set tripal_jbrowse_mgmt_settings "{\"data_dir\":\"$DRUPAL_HOME/$drupalsitedir/sites/default/files/jbrowse/data\",\"data_path\":\"$drupalsitedir/sites/default/files/jbrowse/data\",\"bin_path\":\"$DRUPAL_HOME\/$drupalsitedir/tools/jbrowse/bin\",\"link\":\"tools/jbrowse\",\"menu_template\":[]}" --root="$DRUPAL_HOME"/"$drupalsitedir"
+
+# User intervention
 whiptail --title "JBrowse Example Data (Volvox)" --msgbox --ok-button "OK" --notags "\nTo see JBrowse example data, go to http://localhost/""$drupalsitedir""/tools/jbrowse/index.html?data=sample_data/json/volvox\n\nTo exit, hit 'OK'." 11 80
+
