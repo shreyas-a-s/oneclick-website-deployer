4de1ea5865b9e3e31c52833ef0c91f32739d6138
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-27 14:39:08 +0530

    Improve documentation and add better shebang to 4-drupal-install.sh

--- scripts/deployment/4-drupal-install.sh
+++ scripts/deployment/4-drupal-install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Display task name
 printf '+-------------------------+
@@ -19,7 +19,7 @@ if [[ -z ${psqldb} ]]; then
   . ./components/get-db-creds.sh
 fi
 
-# Installation
+# Install Drupal
 sudo chown -R "$USER" "$DRUPAL_HOME"
 cd "$DRUPAL_HOME" || exit
 mv index.html index.html.orig
@@ -27,19 +27,27 @@ wget http://ftp.drupal.org/files/projects/drupal-"$latest_drupal_seven_version".
 tar -zxvf drupal-"$latest_drupal_seven_version".tar.gz
 rm drupal-"$latest_drupal_seven_version".tar.gz
 mv drupal-"$latest_drupal_seven_version"/ "$drupalsitedir"/
+
+# Setup Drupal
 cd "$drupalsitedir" || exit
 cp sites/default/default.settings.php sites/default/settings.php
 mkdir sites/default/files/
 sudo chgrp -R www-data sites/default/files/
 sudo chmod 777 sites/default/settings.php
 sudo chmod g+rw sites/default/files/
+
+# Write database credentials to Drupal settings file
+sed -i "s/\$databases\ =\ array();/\n\$databases['default']['default']\ =\ array(\n\t'driver'\ =>\ 'pgsql',\n\t'database'\ =>\ '$psqldb',\n\t'username'\ =>\ '$psqluser',\n\t'password'\ =>\ '$ESCAPED_PGPASSWORD',\n\t'host'\ =>\ 'localhost',\n\t'prefix'\ =>\ '',\n);/" sites/default/settings.php
+
+# Manual Setup of Drupal
 echo -e '\n+----------------+'
 echo '|   Site Setup   |'
 echo '+----------------+'
-sed -i "s/\$databases\ =\ array();/\n\$databases['default']['default']\ =\ array(\n\t'driver'\ =>\ 'pgsql',\n\t'database'\ =>\ '$psqldb',\n\t'username'\ =>\ '$psqluser',\n\t'password'\ =>\ '$ESCAPED_PGPASSWORD',\n\t'host'\ =>\ 'localhost',\n\t'prefix'\ =>\ '',\n);/" sites/default/settings.php
 while ! ({ drush variable-get --root="$DRUPAL_HOME"/"$drupalsitedir" | grep -q drupal; } &> /dev/null); do
   whiptail --title "Drupal Setup" --msgbox --ok-button "OK" --notags "1. Go to http://localhost/""$drupalsitedir""/install.php\n2. Ensure that 'Standard' option is selected and click 'Save and continue'\n3. Select the language you want to choose. Choose 'English'.\n4. You will see a progress bar as Drupal is installed.\n5. Once it completes, a configuration page will be visible.\n6. Provide details appropriate to your site and note down the Site Maintenance Account details.\n7. Click 'Save and continue.'\n8. Hit 'OK' after completing these steps." 15 78;
 done
+
+# Limit write permission to owner of 'settings.php' for security reasons
 sudo chmod 755 sites/default/settings.php
 
 # Install Webform Module (for creating Google-form-like Forms)
