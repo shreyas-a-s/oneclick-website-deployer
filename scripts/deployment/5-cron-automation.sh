ef025dbd6fcbb1b7b5aa5640eed3145150954298
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-27 14:50:24 +0530

    Separate cron key into variable

--- scripts/deployment/5-cron-automation.sh
+++ scripts/deployment/5-cron-automation.sh
@@ -13,9 +13,12 @@ if [[ -z ${drupalsitedir} ]]; then
   read -r -p "Enter the name of the directory in which drupal website was installed: " drupalsitedir
 fi
 
+# Store drupal cron key into new variable
+drupal_cron_key=$(drush variable-get cron_key --root="$DRUPAL_HOME"/"$drupalsitedir" | awk '{print $2}')
+
 # Installation
-echo "0,30 * * * * /usr/bin/wget -O - -q http://localhost/""$drupalsitedir""/cron.php?cron_key=""$(drush variable-get cron_key --root="$DRUPAL_HOME"/"$drupalsitedir" | awk '{print $2}')" | sudo tee /etc/cron.d/drupal-cron-tasks > /dev/null
+echo "0,30 * * * * /usr/bin/wget -O - -q http://localhost/""$drupalsitedir""/cron.php?cron_key=""$drupal_cron_key" | sudo tee /etc/cron.d/drupal-cron-tasks > /dev/null
 whiptail --title "Cron Run" --msgbox --ok-button "OK" --notags "1. Go to http://localhost/""$drupalsitedir""/admin/config/system/cron\n2. Set the drop down value titled 'Run cron every' to 'Never' and save the configuration.\n3. Hit 'OK' after completing these steps." 10 65
-if wget -O - -q http://localhost/"$drupalsitedir"/cron.php?cron_key="$(drush variable-get cron_key --root="$DRUPAL_HOME"/"$drupalsitedir" | awk '{print $2}')"; then
+if wget -O - -q http://localhost/"$drupalsitedir"/cron.php?cron_key="$drupal_cron_key"; then
   echo "Cron Setup Successful."
 fi
