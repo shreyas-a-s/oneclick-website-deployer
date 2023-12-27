bb3ff5fe8ecae362070929a33386b63bf7b13e4e
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-27 15:39:03 +0530

    Minor re-arrangement

--- scripts/deployment/7-daemon-install.sh
+++ scripts/deployment/7-daemon-install.sh
@@ -13,16 +13,22 @@ if [[ -z ${drupalsitedir} ]]; then
 	read -r -p "Enter the name of the directory in which drupal website was installed: " drupalsitedir
 fi
 
-# Installation
+# Change into drupal directory
 cd "$DRUPAL_HOME"/"$drupalsitedir"/ || exit
+
+# Install dependencies
 drush pm-download libraries -y
-drush pm-enable libraries -y
 wget -O sites/all/libraries/PHP-Daemon.tar.gz https://github.com/shaneharter/PHP-Daemon/archive/v2.0.tar.gz
 tar -zxvf sites/all/libraries/PHP-Daemon.tar.gz -C sites/all/libraries/
 mv sites/all/libraries/PHP-Daemon-2.0 sites/all/libraries/PHP-Daemon
 rm sites/all/libraries/PHP-Daemon.tar.gz
 drush pm-download drushd -y
-drush pm-enable drushd tripal_daemon -y
+
+# Enable dependencies
+drush pm-enable -y libraries drushd
+
+# Enable tripal daemon
+drush pm-enable tripal_daemon -y
 
 # Fix for "Trying to access array offset on value of type null in PHP-Daemon/Core/error_handlers.php on line 118"
 sed -i "/is_array/ c\    if\ (\!is_array(\$error)\ ||\ \!isset(\$error['type']))" sites/all/libraries/PHP-Daemon/Core/error_handlers.php
@@ -32,3 +38,4 @@ drush trpjob-daemon start
 
 # Set daemon to autostart during boot
 echo "@reboot $USER /usr/local/bin/drush trpjob-daemon start --root=$DRUPAL_HOME/$drupalsitedir" | sudo tee /etc/cron.d/tripal-daemon-autostart > /dev/null
+
