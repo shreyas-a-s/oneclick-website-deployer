1015a0780bd086366258d407ac3fbe60585adfe0
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-27 15:22:25 +0530

    Separate prepare-chado.sh from install-tripal

--- scripts/deployment/6-tripal-install.sh
+++ scripts/deployment/6-tripal-install.sh
@@ -38,37 +38,7 @@ _test_sma_username
 ."$SCRIPT_DIR"/components/install-chado.sh
 
 # Chado preparation
-echo -e '\n+---------------------+'
-echo '|   Preparing Chado   |'
-echo '+---------------------+'
-
-initial_network_config=$(ip -o address | grep --invert-match lo)
-
-while true; do
-  goodtogo=true
-  _test_raw_github
-  if [ "$goodtogo" = false ]; then
-    # Ask the user if they want to try different network setup
-    if (whiptail --title "Unable to proceed" --yesno --yes-button "Retry" --no-button "Continue" "\n- Unable to connect to raw.githubusercontent.com.\n- For preparing website with chado, connecting to it\n  is necessary.\n- You can 'Continue' if you want but it is advisable\n  to change network configuration and 'Retry'." 13 57) then
-      continue
-    else
-      break
-    fi
-  else
-    break
-  fi
-done
-drush trp-prepare-chado --user="$smausername" --root="$DRUPAL_HOME"/"$drupalsitedir"
-drush cache-clear all --root="$DRUPAL_HOME"/"$drupalsitedir"
-
-current_network_config=$(ip -o address | grep --invert-match lo)
-
-# Promt user if they want to change network back
-if [ "$current_network_config" != "$initial_network_config" ]; then
-  whiptail --title "Just a Pause" --msgbox "\n- We've noticed that you've switched network before.\n- This would be agood time to change it back if you wish.\n- Do that and click 'Ok'." 11 61
-  whiptail --title "Just checking" --msgbox --ok-button "Yes" "         Are you sure?" 8 35
-  while ! ({ ping -c 1 -w 2 example.org; } &> /dev/null); do :; done
-fi
+."$SCRIPT_DIR"/components/prepare-chado.sh
 
 # Fix for "Trying to access array offset on value of type null" error that gets displayed
 # when we refresh overlay menus (eg: localhost/drupal/bio_data/1#overlay-context=&overlay=admin/tripal)
---
+++ scripts/deployment/components/prepare-chado.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Display title
+echo -e '\n+---------------------+'
+echo '|   Preparing Chado   |'
+echo '+---------------------+'
+
+initial_network_config=$(ip -o address | grep --invert-match lo)
+
+while true; do
+  goodtogo=true
+  _test_raw_github
+  if [ "$goodtogo" = false ]; then
+    # Ask the user if they want to try different network setup
+    if (whiptail --title "Unable to proceed" --yesno --yes-button "Retry" --no-button "Continue" "\n- Unable to connect to raw.githubusercontent.com.\n- For preparing website with chado, connecting to it\n  is necessary.\n- You can 'Continue' if you want but it is advisable\n  to change network configuration and 'Retry'." 13 57) then
+      continue
+    else
+      break
+    fi
+  else
+    break
+  fi
+done
+drush trp-prepare-chado --user="$smausername" --root="$DRUPAL_HOME"/"$drupalsitedir"
+drush cache-clear all --root="$DRUPAL_HOME"/"$drupalsitedir"
+
+current_network_config=$(ip -o address | grep --invert-match lo)
+
+# Promt user if they want to change network back
+if [ "$current_network_config" != "$initial_network_config" ]; then
+  whiptail --title "Just a Pause" --msgbox "\n- We've noticed that you've switched network before.\n- This would be agood time to change it back if you wish.\n- Do that and click 'Ok'." 11 61
+  whiptail --title "Just checking" --msgbox --ok-button "Yes" "         Are you sure?" 8 35
+  while ! ({ ping -c 1 -w 2 example.org; } &> /dev/null); do :; done
+fi
+
