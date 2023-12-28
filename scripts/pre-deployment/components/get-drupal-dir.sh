6e0c7d90e65c505d7d838da498e727a0096c25c4
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-28 14:57:18 +0530

    Revert "Fix typo when exporting drupalsitedir variable - lack of $"
    This reverts commit d831f9d470e7fa82a165cd5761258ab5c28f1e23.

--- scripts/pre-deployment/components/get-drupal-dir.sh
+++ scripts/pre-deployment/components/get-drupal-dir.sh
@@ -2,5 +2,5 @@
 
 drupalsitedir=$(whiptail --title "User Input" --inputbox "\nEnter the name of the directory to which drupal website needs to be installed: " 10 45 "$drupalsitedir" 3>&1 1>&2 2>&3)
 
-export "$drupalsitedir"
+export drupalsitedir
 
