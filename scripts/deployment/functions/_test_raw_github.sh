40b9974c13552d7a68f27e7f54bac1ad2223ad99
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-28 23:47:22 +0530

    Add testing raw github verbose

--- scripts/deployment/functions/_test_raw_github.sh
+++ scripts/deployment/functions/_test_raw_github.sh
@@ -2,6 +2,8 @@
 
 # Test if raw.githubusercontent.com is accessible or not
 function _test_raw_github {
+  printf "Testing if raw/githubusercontent.com is accessible.\n"
+  printf "Please wait.."
   for _ in {1..6}
     do
       if ! wget --timeout=3 --tries=1 --spider -q http://purl.obolibrary.org/obo/taxrank.obo; then
