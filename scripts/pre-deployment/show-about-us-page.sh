1f8320d62dbb3e996efc0ad07f86cd579900d9d2
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-28 15:10:10 +0530

    Fix - about-us.txt not found

--- scripts/pre-deployment/show-about-us-page.sh
+++ scripts/pre-deployment/show-about-us-page.sh
@@ -4,16 +4,16 @@
 SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 
 if command -v whiptail > /dev/null; then # Display 'About Us' using whiptail
-  if [ -f ./about-us.txt ]; then # Checking is about.txt is present
-    whiptail --title "about-us The Script" --textbox --ok-button "Continue" ./about.txt 20 86
+  if [ -f ./about-us.txt ]; then # Checking is about-us.txt is present
+    whiptail --title "about-us The Script" --textbox --ok-button "Continue" ./about-us.txt 20 86
   else
-    echo "about.txt is not found"
+    echo "about-us.txt is not found"
   fi
 else # Display 'About Us' using command-line
-  if [ -f ./about-us.txt ]; then # Checking is about.txt is present
+  if [ -f ./about-us.txt ]; then # Checking is about-us.txt is present
     cat ./about-us.txt
   else
-    echo "about.txt is not found"
+    echo "about-us.txt is not found"
   fi
 fi
 
