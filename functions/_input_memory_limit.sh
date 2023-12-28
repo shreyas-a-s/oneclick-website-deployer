4c2966fe5e1977f52d589708493b8416b03a08ef
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-29 02:07:20 +0530

    Simplify by moving non-whiptail memorylimit-get from install-postgres to memorylimit function

--- functions/_input_memory_limit.sh
+++ functions/_input_memory_limit.sh
@@ -1,25 +1,45 @@
 #!/usr/bin/env bash
 
 function _input_memory_limit {
-  memory_limit_title="User Input"
-  memory_limit_msg=""
-  total_memory="$(command free --mega | awk 'NR==2{print $2}')"
+  if command -v whiptail > /dev/null; then
+    memory_limit_title="User Input"
+    memory_limit_msg=""
+    total_memory="$(command free --mega | awk 'NR==2{print $2}')"
 
-  while true; do
-    memorylimit=$(whiptail --title "$memory_limit_title" --inputbox "\n$memory_limit_msg""How much memory to allocate to the website (in MB)? " 11 46 "$memorylimit" 3>&1 1>&2 2>&3)
-    if [ "$memorylimit" -ne "$memorylimit" ] &> /dev/null; then
-      memory_limit_title="ERROR"
-      memory_limit_msg="Only integer values are accepted.\n"
-      continue
-    elif [ "$memorylimit" -ge "$total_memory" ]; then
-      memory_limit_title="ERROR"
-      memory_limit_msg="Value is Larger than Total RAM ($total_memory""MB).\n"
-      continue
-    else
-      break
-    fi
-  done
-
+    while true; do
+      memorylimit=$(whiptail --title "$memory_limit_title" --inputbox "\n$memory_limit_msg""How much memory to allocate to the website (in MB)? " 11 46 "$memorylimit" 3>&1 1>&2 2>&3)
+      if [ "$memorylimit" -ne "$memorylimit" ] &> /dev/null; then
+        memory_limit_title="ERROR"
+        memory_limit_msg="Only integer values are accepted.\n"
+        continue
+      elif [ "$memorylimit" -ge "$total_memory" ]; then
+        memory_limit_title="ERROR"
+        memory_limit_msg="Value is Larger than Total RAM ($total_memory""MB).\n"
+        continue
+      else
+        break
+      fi
+    done
+  else
+    memory_limit_title=""
+    memory_limit_msg=""
+    total_memory="$(command free --mega | awk 'NR==2{print $2}')"
+    while true; do # This while loop enables checking if memory limit entered valid
+      printf "$memory_limit_title\n$memory_limit_msg""How much memory to allocate to the website? (in MB): "
+      read -r memorylimit
+      if [ "$memorylimit" -ne "$memorylimit" ] &> /dev/null; then
+        memory_limit_title='\nERROR!! '
+        memory_limit_msg="Only integer values are accepted.\n"
+        continue
+      elif [ "$memorylimit" -ge "$total_memory" ]; then
+        memory_limit_title='\nERROR!! '
+        memory_limit_msg="Value is Larger than Total RAM ($total_memory""MB).\n"
+        continue
+      else
+        break
+      fi
+    done
+  fi
   export memorylimit
 }
 
