d365fbb64880de8f5921f28de17e5367417c8f25
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-28 15:11:48 +0530

    Fix title of about-us whiptail box

--- scripts/pre-deployment/show-about-us-page.sh
+++ scripts/pre-deployment/show-about-us-page.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 
 if command -v whiptail > /dev/null; then # Display 'About Us' using whiptail
   if [ -f ./about-us.txt ]; then # Checking is about-us.txt is present
-    whiptail --title "about-us The Script" --textbox --ok-button "Continue" ./about-us.txt 20 86
+    whiptail --title "About The Script" --textbox --ok-button "Continue" ./about-us.txt 20 86
   else
     echo "about-us.txt is not found"
   fi
