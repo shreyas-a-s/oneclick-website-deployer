3e53854ad24a170e68d43ca5d30109b5d66d0dab
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-28 14:37:22 +0530

    Add full path to drupal sub dirs

--- scripts/deployment/components/set-drupal-tmp-path.sh
+++ scripts/deployment/components/set-drupal-tmp-path.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Set new drupal file_temporary_path (Done as a replacement for setting PrivateTmp=false)
-mkdir -p sites/default/files/tmp/
-sudo chgrp www-data sites/default/files/tmp/
-chmod g+w sites/default/files/tmp/
+mkdir -p "$DRUPAL_HOME"/"$drupalsitedir"/sites/default/files/tmp/
+sudo chgrp www-data "$DRUPAL_HOME"/"$drupalsitedir"/sites/default/files/tmp/
+chmod g+w "$DRUPAL_HOME"/"$drupalsitedir"/sites/default/files/tmp/
 drush variable-set file_temporary_path "$DRUPAL_HOME"/"$drupalsitedir"/sites/default/files/tmp --root="$DRUPAL_HOME"/"$drupalsitedir"
 
