a9a236e04b0269b88f9fbc17b116a27c37bac479
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-29 15:44:26 +0530

    Minor edits

--- scripts/deployment/functions/_test_raw_github.sh
+++ scripts/deployment/functions/_test_raw_github.sh
@@ -3,13 +3,14 @@
 # Test if raw.githubusercontent.com is accessible or not
 function _test_raw_github {
   printf "Testing if raw.githubusercontent.com is accessible.\n"
-  printf "Please wait..\n"
+  printf "Please wait.."
   for _ in {1..6}
     do
       if ! wget --timeout=3 --tries=1 --spider -q http://purl.obolibrary.org/obo/taxrank.obo; then
 	export goodtogo=false
       fi
   done
+  printf "\n\n"
 }
 
 export -f _test_raw_github
