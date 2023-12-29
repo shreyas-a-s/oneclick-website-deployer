7c1123df8734d8d755573b715934bf991d80957d
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-30 02:46:16 +0530

    Add whiptail box to function _is_internet_available

--- functions/_is_internet_available.sh
+++ functions/_is_internet_available.sh
@@ -3,7 +3,11 @@
 function _is_internet_available {
   printf "Checking internet connectivity. Please wait.."
   if ! ping -c 3 1.1.1.1 &> /dev/null; then
-    printf "\nUnable to connect to internet. Check and make sure you are connected to internet before executing the script.\n"
+    if command -v whiptail > /dev/null; then
+      whiptail --title "WARNING" --msgbox "\nUnable to connect to internet. Check and make sure you are connected to internet before executing the script.\n" 9 44
+    else
+      printf "\nUnable to connect to internet. Check and make sure you are connected to internet before executing the script.\n"
+    fi
     exit 1
   fi
 }
