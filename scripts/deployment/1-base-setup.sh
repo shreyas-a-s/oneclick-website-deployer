d8a5f9487fad2fdbc038471a0ea5c0777de97cd0
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-29 16:19:58 +0530

    Move php-errors.log setup from 2-drush-install.sh to 1-base-setup.sh & removed setting chown $USER for it

--- scripts/deployment/1-base-setup.sh
+++ scripts/deployment/1-base-setup.sh
@@ -17,6 +17,8 @@ cd /etc/apache2/mods-enabled || exit
 sudo ln -s ../mods-available/rewrite.load # Enable Rewrite module for apache
 sudo sed -i '$i<Directory /var/www/html>\n   Options Indexes FollowSymLinks MultiViews\n   AllowOverride All\n   Order allow,deny\n   allow from all\n</Directory>' /etc/apache2/sites-available/000-default.conf # Set web root directory
 sudo sed -i "/memory_limit/ c\memory_limit = $memorylimit\M" /etc/php/"$php_version"/apache2/php.ini # Set max amount of RAM a PHP script can consume
+sudo touch /var/log/php-errors.log # Fix for "The PHP error_log at  is not writable!"
+sudo sed -i "/error_log\ =\ php/ c\error_log\ =\ /var/log/php-errors.log" /etc/php/"$php_version"/cli/php.ini
 
 # Restart apache for the new configurations to take effect
 sudo service apache2 restart
--- scripts/deployment/2-drush-install.sh
+++ scripts/deployment/2-drush-install.sh
@@ -5,11 +5,6 @@ echo -e '\n+------------------------+'
 echo '|   Drush Installation   |'
 echo '+------------------------+'
 
-# Read php version
-if command -v apt-cache > /dev/null; then
-  php_version=$(apt-cache show php | grep version | awk '{print $4}' | awk -F ')' '{print $1}')
-fi
-
 # Install wget (dependency of this script)
 if ! command -v wget > /dev/null; then
   ./components/install-wget.sh
@@ -19,9 +14,4 @@ fi
 wget https://github.com/drush-ops/drush/releases/download/8.4.12/drush.phar
 chmod +x drush.phar
 sudo mv drush.phar /usr/local/bin/drush
-
-# Fix for "The PHP error_log at  is not writable!"
-sudo touch /var/log/php-errors.log
-sudo chown "$USER" /var/log/php-errors.log
-sudo sed -i "/error_log\ =\ php/ c\error_log\ =\ /var/log/php-errors.log" /etc/php/"$php_version"/cli/php.ini
 
