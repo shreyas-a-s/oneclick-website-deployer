b8afd6031112c5935c200b78a0f0110ff2c6ec75
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-30 00:12:11 +0530

    Incorporate prepare-chado.sh into 6-tripal-install.sh

--- scripts/deployment/6-tripal-install.sh
+++ scripts/deployment/6-tripal-install.sh
@@ -29,7 +29,6 @@ drush pm-enable -y tripal tripal_chado tripal_ds tripal_ws
 # Check site maintenance username before proceeding
 _input_site_maintenance_username
 
-"$SCRIPT_DIR"/components/prepare-chado.sh  # Chado preparation
 # Install Tripal chado
 echo -e '\n+----------------------+'
 echo '|   Installing Chado   |'
@@ -45,6 +44,42 @@ if [ $? -eq 0 ]; then
   echo "Chado Installation Successful."
 else
   echo "Chado Installation Failed."
+fi
+
+# Prepare Tripal Chado
+echo -e '\n+---------------------+'
+echo '|   Preparing Chado   |'
+echo '+---------------------+'
+
+# Save initial network configuration into a string
+initial_network_config=$(ip addr show | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+
+# Actual execution
+while true; do
+  goodtogo=true
+  _test_raw_github
+  if [ "$goodtogo" = false ]; then
+    # Ask the user if they want to try different network setup
+    if (whiptail --title "Unable to proceed" --yesno --yes-button "Retry" --no-button "Continue" "\n- Unable to connect to raw.githubusercontent.com.\n- For preparing website with chado, connecting to it\n  is necessary.\n- You can 'Continue' if you want but it is advisable\n  to change network configuration and 'Retry'." 13 57) then
+      continue
+    else
+      break
+    fi
+  else
+    break
+  fi
+done
+drush trp-prepare-chado --user="$smausername" --root="$DRUPAL_HOME"/"$drupalsitedir"
+drush cache-clear all --root="$DRUPAL_HOME"/"$drupalsitedir"
+
+# Save current network configuration into a string
+current_network_config=$(ip addr show | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+
+# If network change is detected, promt user if they want to change network back
+if [ "$current_network_config" != "$initial_network_config" ]; then
+  whiptail --title "Just a Pause" --msgbox "\n- We've noticed that you've switched network before.\n- This would be a good time to change it back if you wish.\n- Do that and click 'Ok'." 11 61
+  whiptail --title "Just checking" --msgbox --ok-button "Yes" "         Are you sure?" 8 35
+  while ! ({ ping -c 1 -w 2 example.org; } &> /dev/null); do :; done
 fi
 
 # Fix for "Trying to access array offset on value of type null" error that gets displayed
