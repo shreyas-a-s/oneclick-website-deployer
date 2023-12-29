eba9642a7b7daf48df24c5e8e1b2eca5689099d0
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-29 15:11:02 +0530

    Make about-us page show even if whiptail is not installed

--- show-about-us-page.sh
+++ show-about-us-page.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
+# Input file
+input_file="./about-us.txt"
+
 # Change directory
 SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 
 if command -v whiptail > /dev/null; then # Display 'About Us' using whiptail
-  if [ -f ./about-us.txt ]; then # Checking is about-us.txt is present
-    whiptail --title "About The Script" --textbox --ok-button "Continue" ./about-us.txt 20 86
-  else
-    echo "about-us.txt is not found"
-  fi
+  whiptail --title "About The Script" --textbox --ok-button "Continue" $input_file 20 86
 else # Display 'About Us' using command-line
-  if [ -f ./about-us.txt ]; then # Checking is about-us.txt is present
-    cat ./about-us.txt
-  else
-    echo "about-us.txt is not found"
-  fi
+  number_of_columns=$(tput cols)
+  line_char_count=$(( $(awk 'NR==1{print length}' "$input_file") - 2 ))
+  left_padding_char_count=$(((number_of_columns - line_char_count) / 2 - 2))
+  left_padding=$(for _ in $(seq $left_padding_char_count); do printf ' '; done)
+  sed "s/^/$left_padding/" $input_file
+  printf "Press Any key to continue.."
+  read -r
 fi
 
