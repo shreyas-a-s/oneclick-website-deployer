2a8239c1e9f90705212c112abd152f008d01a048
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-31 02:07:56 +0530

    Make whiptail msg in _input_drupal_dir.sh more clearer

--- functions/_input_drupal_dir.sh
+++ functions/_input_drupal_dir.sh
@@ -3,8 +3,9 @@
 function _input_drupal_dir {
   if command -v whiptail > /dev/null; then
     drupalsitedir=$(whiptail --title "USER INPUT" --inputbox \
-      "\nEnter the name of the directory to which drupal website needs to be installed:" \
-      10 45 \
+      "\nEnter the name of the directory to which\n  drupal website needs to be installed:\
+      \n        (Press ENTER to continue)" \
+      12 45 \
       "$drupalsitedir" \
       3>&1 1>&2 2>&3)
   else
