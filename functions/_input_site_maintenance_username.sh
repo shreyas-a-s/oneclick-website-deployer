45b960719c1cd41165885c94087c32e89ed34dfd
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-31 01:55:18 +0530

    Make whiptail msg in _input_site_maintenance_username.sh more clearer

--- functions/_input_site_maintenance_username.sh
+++ functions/_input_site_maintenance_username.sh
@@ -5,8 +5,9 @@ function _input_site_maintenance_username {
   titleSMA="SITE MAINTENANCE ACCOUNT CREDENTIALS"
   while ! ( { drush user-information "$smausername" --root="$DRUPAL_HOME"/"$drupalsitedir" | grep -q administrator; } &> /dev/null ); do
     smausername=$(whiptail --title "$titleSMA" --inputbox \
-      "\nEnter the site maintenance account username that you've given to the website:" \
-      10 48 \
+      "\nEnter the site maintenance account username that you've given to the website:\
+      \n\n         (Press ENTER to continue)" \
+      12 48 \ 
       3>&1 1>&2 2>&3)
       titleSMA="WRONG USERNAME"
       _set_whiptail_colors_red_bg # Change whiptail bg color to RED
