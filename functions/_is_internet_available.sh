7b22e3c30f654aca7e426c3fd62ebd6e99d455b7
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-31 01:53:30 +0530

    Make whiptail msg in _is_internet_available.sh more clearer

--- functions/_is_internet_available.sh
+++ functions/_is_internet_available.sh
@@ -6,8 +6,9 @@ function _is_internet_available {
     if command -v whiptail > /dev/null; then
       _set_whiptail_colors_red_bg # Change whiptail bg color to RED
       whiptail --title "WARNING" --msgbox \
-        "Unable to connect to internet. Check and make sure you are connected to internet before executing the script." \
-        9 44
+        "Unable to connect to internet. Check and make sure you are connected to internet before executing the script.\
+        \n\n         (Press ENTER to exit)" \
+        11 44
       _set_whiptail_colors_default # Restore default colorscheme
     else
       printf "\nUnable to connect to internet. Check and make sure you are connected to internet before executing the script.\n"
