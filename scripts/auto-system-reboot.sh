0350c8c1624a164695fcf49ec2639cd1bdd47954
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-31 00:37:31 +0530

    Make whiptail msg in auto-system-reboot.sh more clearer

--- scripts/auto-system-reboot.sh
+++ scripts/auto-system-reboot.sh
@@ -6,7 +6,11 @@ if type _printtitle &> /dev/null; then
 fi
 
 if command -v whiptail > /dev/null; then
-  whiptail --title "INSTALLATION COMPLETE" --yesno --no-button "Later" "Reboot the system for site installation to fully take effect. Reboot now?" 8 44
+  whiptail --title "INSTALLATION COMPLETE" --yesno \
+  --no-button "Later" \
+  "Reboot the system for site installation\n   to fully take effect. Reboot now? \
+  \n (ARROW KEYS to move, ENTER to confirm)" \
+  9 44
   exitstatus=$?
   if [ $exitstatus = 0 ]; then
     sudo reboot
