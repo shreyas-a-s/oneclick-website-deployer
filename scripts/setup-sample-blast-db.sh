c5fc9ad16e80654b27ecf3c69e39e2e237b99e6c
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-31 01:22:35 +0530

    Make whiptail msg in setup-sample-blast-db.sh.sh more clearer

--- scripts/setup-sample-blast-db.sh
+++ scripts/setup-sample-blast-db.sh
@@ -18,6 +18,17 @@ if [ ! -f "$DRUPAL_HOME/$drupalsitedir/tools/blast/db/16S_ribosomal_RNA/16S_ribo
 fi
 
 # User Intervention
-whiptail --title "SAMPLE BLAST DB SETUP" --msgbox --ok-button "OK" --notags "1. Go to http://localhost/""$drupalsitedir""/node/add/blastdb\n2. Fill out the form like this:\n- Human-readable Name: 16S_ribosomal_RNA\n- File Prefix including Full Path: ""$DRUPAL_HOME""/""$drupalsitedir""/tools/blast/db/16S_ribosomal_RNA/16S_ribosomal_RNA\n- Set 'Type of the blast database' to Nucleotide.\n3. Got to bottom of page and click 'Save'\n4. After the installation is completed and system is rebooted, you can go to http://localhost/""$drupalsitedir""/blast/nucleotide/nucleotide and test out blast by Entering a FASTA sequence (or uploading one) & Selecting the newly added Database from dropdown under Nucleotide BLAST Databases & Clicking 'BLAST'\n5. Hit 'OK' after completing these steps." 18 78
+whiptail --title "SAMPLE BLAST DB SETUP" --msgbox \
+  --ok-button "OK" \
+  --notags \
+  "1. Go to http://localhost/""$drupalsitedir""/node/add/blastdb\
+  \n2. Fill out the form like this:\
+  \n- Human-readable Name: 16S_ribosomal_RNA\
+  \n- File Prefix including Full Path: ""$DRUPAL_HOME""/""$drupalsitedir""/tools/blast/db/16S_ribosomal_RNA/16S_ribosomal_RNA\
+  \n- Set 'Type of the blast database' to Nucleotide.\
+  \n3. Got to bottom of page and click 'Save'\
+  \n4. After installation is completed and system is rebooted, you can go to http://localhost/""$drupalsitedir""/blast/nucleotide/nucleotide and test out blast by Entering a FASTA sequence (or uploading one) & Selecting the newly added Database from dropdown under Nucleotide BLAST Databases & Clicking 'BLAST'\
+  \n5. Press ENTER to continue." \
+  19 78
 echo "Setup complete."
 
