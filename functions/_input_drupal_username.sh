97aea599a1eff16266d3db767c88793c61bbc558
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-07 02:15:18 +0530

    Correct the message in fn _input_drupal_username

--- functions/_input_drupal_username.sh
+++ functions/_input_drupal_username.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-# Read and test site maintenance account username is valid
 function _input_drupal_username {
-  drupal_user=$(whiptail --title "SITE MAINTENANCE ACCOUNT CREDENTIALS" --inputbox \
-    "\nEnter the site maintenance account username that you've given to the website:\
-    \n\n         (Press ENTER to continue)" \
+  drupal_user=$(whiptail --title "DRUPAL WEBSITE DETAILS" --inputbox \
+    "\nEnter the username to be used for drupal:\
+    \n         (Press ENTER to continue)" \
     12 48 \
     3>&1 1>&2 2>&3)
   export drupal_user # Export smausername to be used by child scripts
