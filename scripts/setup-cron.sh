9bf61c095258eef1b0ac2a43ee556a78cd3ab92e
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-07 21:14:59 +0530

    Automate cron setup (no need of manual intervention anymore)

--- scripts/setup-cron.sh
+++ scripts/setup-cron.sh
@@ -5,23 +5,22 @@ if type _printtitle &> /dev/null; then
   _printtitle "SETTING UP - CRON"
 fi
 
-# Store drupal cron key into new variable
-drupal_cron_key=$(drush variable-get cron_key --root="$DRUPAL_HOME"/"$drupalsitedir" | awk '{print $2}')
-
-# Set up cronjob to do drupal cron tasks
+# Set up system cronjob to do drupal tasks
 echo "0,30 * * * * /usr/bin/wget -O - -q http://localhost/""$drupalsitedir""/cron.php?cron_key=""$drupal_cron_key" | sudo tee /etc/cron.d/drupal-cron-tasks > /dev/null
 
-# Prompt user to disable drupal's built-in cron otherwise websiste might get slowed down
-whiptail --title "CRON SETUP" --msgbox \
-  --ok-button "OK" \
-  --notags \
-  "1. Go to http://localhost/""$drupalsitedir""/admin/config/system/cron\
-  \n2. Set the drop down value titled 'Run cron every' to 'Never' and save the configuration.\
-  \n3. Press ENTER after completing these steps." \
-  11 65
+# Disable drupal cron to prevent website slowing down when users visit
+drush vset cron_safe_threshold 0
 
-# Check if cron setup was a success
-if wget -O - -q http://localhost/"$drupalsitedir"/cron.php?cron_key="$drupal_cron_key"; then
-  echo "Cron Setup Successful."
+# Store exit status to variable
+exitstatus=$?
+
+# Display result of operation
+if [ $exitstatus = 0 ]; then
+  echo "Cron setup Successfull"
+else
+  echo "Cron setup Unsuccessful"
 fi
+
+# Wait two seconds to allow user to read displayed message
+sleep 2
 
