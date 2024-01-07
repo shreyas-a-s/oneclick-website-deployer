7f47c36df2e179d94136fc8c6dde1998b04aa12f
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-08 00:55:01 +0530

    Minor edits

--- functions/_input_drupal_site_name.sh
+++ functions/_input_drupal_site_name.sh
@@ -3,9 +3,9 @@
 function _input_drupal_site_name {
   if command -v whiptail > /dev/null; then
     drupal_site_name=$(whiptail --title "DRUPAL WEBSITE DETAILS" --inputbox \
-      "\nEnter a Name for your Drupal website:\
+      "\nEnter a Name for your Drupal website:\n   (Example: My Beautiful Website)\
       \n      (Press ENTER to continue)" \
-      11 41 \
+      12 41 \
       "$drupal_site_name" \
       3>&1 1>&2 2>&3)
     exitstatus=$?
@@ -13,7 +13,7 @@ function _input_drupal_site_name {
       exit 1
     fi
   else
-    printf "\nEnter a Name for your Drupal website: "
+    printf "\nEnter a Name for your Drupal website(eg: My Beautiful Website) : "
     read -r drupal_site_name
   fi
   export drupal_site_name # Export drupal site name to be used by child scripts
