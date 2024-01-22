852bc9847e4809017d9702266ebc218e5804b8af
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-22 20:07:06 +0530

    Separate preparation of chado into separate script

--- scripts/install-tripal.sh
+++ scripts/install-tripal.sh
@@ -34,50 +34,8 @@ else
   printf "\nChado Installation Failed.\n\n"
 fi
 
-# Save initial network configuration into a string
-initial_network_config=$(dig +short myip.opendns.com @resolver1.opendns.com)
-
-# Prepare Tripal Chado
-while true; do
-  goodtogo=true
-  _is_raw.githubusercontent.com_accessible
-  if [ "$goodtogo" = false ]; then
-    _set_whiptail_colors_bg_red # Change whiptail bg color to RED
-    # Ask the user if they want to try different network setup
-    whiptail --title "UNABLE TO PROCEED" --yesno \
-      --yes-button "Retry" \
-      --no-button "Continue" \
-      "* Unable to connect to raw.githubusercontent.com.\
-      \n* For preparing website with chado, this connection is necessary.\
-      \n* You can 'Continue' if you want but it is advisable to change network configuration and 'Retry'.\
-      \n         (ARROW KEYS to move, ENTER to confirm)" \
-      12 59
-    exitstatus=$?
-    if [ $exitstatus = 0 ]; then
-      continue
-    else
-      _set_whiptail_colors_default # Restore default colorscheme
-      break
-    fi
-  else
-    break
-  fi
-done
-drush trp-prepare-chado --user="$drupal_user" --root="$DRUPAL_HOME"/"$drupalsitedir"
-drush trp-run-jobs --username="$drupal_user" --root="$DRUPAL_HOME"/"$drupalsitedir"
-drush cache-clear all --root="$DRUPAL_HOME"/"$drupalsitedir"
-
-# Save current network configuration into a string
-current_network_config=$(dig +short myip.opendns.com @resolver1.opendns.com)
-
-# If network change is detected, promt user if they want to change network back
-if [ "$current_network_config" != "$initial_network_config" ]; then
-  whiptail --title "JUST A PAUSE" --msgbox \
-    "* We've noticed that you've switched network before.\
-    \n* If you wish to change it back, do that and press ENTER." \
-    9 61
-  while ! ({ curl -Is https://www.google.com | head -n 1 | grep -q '200'; } &> /dev/null); do :; done
-fi
+# Prepare Website to work with chado
+./prepare-chado.sh
 
 # Fix for "Trying to access array offset on value of type null" error that gets displayed
 # when we refresh overlay menus (eg: localhost/drupal/bio_data/1#overlay-context=&overlay=admin/tripal)
