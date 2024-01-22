7390c83149456d242e0fd6de6965f6ecc344d055
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-22 19:55:31 +0530

    Minor comment tweaks

--- scripts/install-psql.sh
+++ scripts/install-psql.sh
@@ -5,14 +5,20 @@ if type _printtitle &> /dev/null; then
   _printtitle "INSTALLING - POSTGRESQL DATABASE"
 fi
 
-# Install dependencies
+# Install postgresql
 if command -v apt-get > /dev/null; then
   sudo apt-get install -y postgresql
 fi
 
-# Setup postgreSQL
-sudo -u postgres createuser "$psqluser" # Create a user
-sudo -u postgres createdb "$psqldb"     # Create a database
-sudo -u postgres psql -c "alter user \"$psqluser\" with encrypted password '$PGPASSWORD';" # Set new password to the user
-sudo -u postgres psql -c "grant all privileges on database \"$psqldb\" to \"$psqluser\" ;"     # Set ownership of database to the user
+# Create a postgresql user
+sudo -u postgres createuser "$psqluser"
+
+# Create database
+sudo -u postgres createdb "$psqldb"
+
+# Set new password to newly created user
+sudo -u postgres psql -c "alter user \"$psqluser\" with encrypted password '$PGPASSWORD';"
+
+# Set ownership of database to newly created user
+sudo -u postgres psql -c "grant all privileges on database \"$psqldb\" to \"$psqluser\" ;"
 
