90917197279369ac7c3633f3568fc76238a63ac3
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-22 20:40:09 +0530

    Simplify setup of sample blastdb

--- scripts/setup-sample-blast-db.sh
+++ scripts/setup-sample-blast-db.sh
@@ -5,24 +5,18 @@ if type _printtitle &> /dev/null; then
   _printtitle "SETTING UP - SAMPLE BLAST DATABASE"
 fi
 
-# Store path to directory in which this script resides into a variable
-SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )")
+# Change direcrtory
+SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 
 # Gathering test database
 mkdir -p "$DRUPAL_HOME"/"$drupalsitedir"/tools/blast/db/16S_ribosomal_RNA
 cd "$DRUPAL_HOME"/"$drupalsitedir"/tools/blast/db/16S_ribosomal_RNA || exit
-echo "Downloading Sample Blat database from NCBI. Please be patient..."
-"$DRUPAL_HOME"/"$drupalsitedir"/tools/blast/bin/update_blastdb.pl --passive --decompress 16S_ribosomal_RNA
-
-# Check if Sample data was downloaded
-if [ ! -f "$DRUPAL_HOME/$drupalsitedir/tools/blast/db/16S_ribosomal_RNA/16S_ribosomal_RNA.ndb" ]; then
-  echo "\nSample data could not be downloaded. Hence aborting Blast Database setup."
-  sleep 3
-  exit # Don't setup if Sample data was not downloaded
-fi
+cp "$SCRIPT_DIR"/blastdb/16S_ribosomal_RNA/* ./
 
 # Automatic blast db setup
 drush php-script "$SCRIPT_DIR"/create-blastdb.php --root="$DRUPAL_HOME"/"$drupalsitedir"
+
+# Store exit status in a variable
 exitstatus=$?
 
 # User Intervention
