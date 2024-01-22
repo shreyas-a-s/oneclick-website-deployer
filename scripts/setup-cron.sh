2a52a770b4b620d3fa33601f5c78ea0cdf498a85
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-22 20:02:09 +0530

    Add system cron job only if successfully disabled drupal cron

--- scripts/setup-cron.sh
+++ scripts/setup-cron.sh
@@ -5,9 +5,6 @@ if type _printtitle &> /dev/null; then
   _printtitle "SETTING UP - CRON"
 fi
 
-# Set up system cronjob to do drupal tasks
-echo "0,30 * * * * /usr/bin/wget -O - -q http://localhost/""$drupalsitedir""/cron.php?cron_key=""$drupal_cron_key" | sudo tee /etc/cron.d/drupal-cron-tasks > /dev/null
-
 # Disable drupal cron to prevent website slowing down when users visit
 drush vset cron_safe_threshold 0 --root="$DRUPAL_HOME"/"$drupalsitedir"
 
@@ -16,6 +13,8 @@ exitstatus=$?
 
 # Display result of operation
 if [ $exitstatus = 0 ]; then
+  # Set up system cronjob to do drupal tasks
+  echo "0,30 * * * * /usr/bin/wget -O - -q http://localhost/""$drupalsitedir""/cron.php?cron_key=""$drupal_cron_key" | sudo tee /etc/cron.d/drupal-cron-tasks > /dev/null
   echo "Cron setup Successfull"
 else
   echo "Cron setup Unsuccessful"
