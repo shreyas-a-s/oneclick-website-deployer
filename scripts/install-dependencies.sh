bde87a5b5700696976f2398b481ffa6144d67f23
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-22 23:30:21 +0530

    Change directory before installing dependencies

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Change directory
+SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
+
 if command -v apt-get > /dev/null; then # Install for debian-based distros
   xargs -a "dependencies.txt" sudo apt-get install -y
 fi
