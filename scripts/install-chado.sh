e2c0dea2e1ece90279d246cfa5c9715b068e0d64
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-23 14:37:56 +0530

    Remove success/fail msg from install-chado.sh

--- scripts/install-chado.sh
+++ scripts/install-chado.sh
@@ -9,14 +9,3 @@ fi
 drush php-eval "module_load_include('inc', 'tripal_chado', 'includes/tripal_chado.install'); tripal_chado_load_drush_submit('Install Chado v1.3');" --username="$drupal_user" --root="$DRUPAL_HOME"/"$drupalsitedir"
 drush trp-run-jobs --username="$drupal_user" --root="$DRUPAL_HOME"/"$drupalsitedir"
 
-# Checking if chado installation was successful
-exitstatus=$?
-if [ $exitstatus -eq 0 ]; then
-  printf "\nChado Installation Successful.\n"
-else
-  printf "\nChado Installation Failed.\n"
-fi
-
-# Wait two seconds to allow user to read displayed message
-sleep 2
-
