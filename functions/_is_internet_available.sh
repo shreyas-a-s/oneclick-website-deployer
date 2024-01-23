da0572af4348e2cd8c7fdb5d78c8ce3829666711
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-23 15:08:28 +0530

    Remove whiptail from _is_internet_available

--- functions/_is_internet_available.sh
+++ functions/_is_internet_available.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 
 function _is_internet_available {
-  printf "Checking internet connectivity. Please wait.."
+  printf "Checking internet connectivity. Please wait ..."
   if ! curl -Is https://www.google.com | head -n 1 | grep -q '200' > /dev/null; then
-    _set_whiptail_colors_bg_red # Change whiptail bg color to RED
-    whiptail --title "WARNING" --msgbox \
-      "Unable to connect to internet. Check and make sure you are connected to internet before executing the script.\
-      \n\n         (Press ENTER to exit)" \
-      11 44
-    _set_whiptail_colors_default # Restore default colorscheme
-    printf "\n"
+    printf "\nUnable to connect to internet. Connect to internet before continuing."
     exit 1
   fi
 }
