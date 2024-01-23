4729d87a14b6b311a80966917668d0d46bee4a51
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-23 14:40:15 +0530

    Fix - pm-enable needs a higher bootstrap level (install-tripal-jbrowse.sh)

--- scripts/install-tripal-jbrowse.sh
+++ scripts/install-tripal-jbrowse.sh
@@ -10,7 +10,7 @@ wget https://github.com/tripal/tripal_jbrowse/archive/refs/tags/7.x-3.0.zip
 unzip -q 7.x-3.0.zip
 rm 7.x-3.0.zip
 mv tripal_jbrowse-7.x-3.0 "$DRUPAL_HOME"/"$drupalsitedir"/sites/all/modules/tripal_jbrowse
-drush pm-enable -y tripal_jbrowse_mgmt tripal_jbrowse_page
+drush pm-enable -y tripal_jbrowse_mgmt tripal_jbrowse_page --root="$DRUPAL_HOME"/"$drupalsitedir"
 
 # Setup tripal_jbrowse
 drush variable-set tripal_jbrowse_mgmt_settings "{\"data_dir\":\"$DRUPAL_HOME/$drupalsitedir/sites/default/files/jbrowse/data\",\"data_path\":\"$drupalsitedir/sites/default/files/jbrowse/data\",\"bin_path\":\"$DRUPAL_HOME\/$drupalsitedir/tools/jbrowse/bin\",\"link\":\"tools/jbrowse\",\"menu_template\":[]}" --root="$DRUPAL_HOME"/"$drupalsitedir"
