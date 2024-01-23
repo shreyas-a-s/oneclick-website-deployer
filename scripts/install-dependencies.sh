d34b4e609ba23f930a0f4ebb12b4a9aa70674c36
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-23 13:55:12 +0530

    Refresh repos before installing dependencies

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -4,6 +4,7 @@
 SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 
 if command -v apt-get > /dev/null; then # Install for debian-based distros
+  sudo apt-get update
   xargs -a "../components/dependencies.txt" sudo apt-get install -y
 fi
 
