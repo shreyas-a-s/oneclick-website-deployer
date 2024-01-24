55b0f2859e298759272d8b92fb53796a8e0fac4f
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-24 19:44:24 +0530

    Minor tweaks

--- functions/_is_internet_available.sh
+++ functions/_is_internet_available.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 function _is_internet_available {
-  printf "Checking internet connectivity. Please wait ..."
+  printf "Checking internet connectivity. Please wait ...\n"
   if ! curl -Is https://www.google.com | head -n 1 | grep -q '200' > /dev/null; then
-    printf "\nUnable to connect to internet. Connect to internet before continuing."
+    printf "\nUnable to connect to internet. Connect to internet before continuing.\n"
     exit 1
   fi
 }
